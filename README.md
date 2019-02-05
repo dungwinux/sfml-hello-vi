8b62e464c94552919e8340fb01406e8c6a5bf32f
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2019-02-05 13:18:33 +0700

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # sfml-hello-vi
-Chương trình "Hello, World !" viết bằng C++ &amp; sử dụng thư viện SFML 2.5.1
+Chương trình "Hello, World !" viết bằng C++ &amp; sử dụng thư viện SFML 2.5+
+
+- SFML: https://www.sfml-dev.org/
+- `SourceSansPro-Regular.otf` - sử dụng dưới giấy phép OFL (SIL Open Font License): https://scripts.sil.org/OFL
---
+++ main.cpp
@@ -0,0 +1,33 @@
+﻿#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+int main()
+{
+    std::wstring newYearText = L"Xin chào, thế giới !";
+    sf::RenderWindow window(sf::VideoMode(640, 480), newYearText);
+
+    sf::Font font;
+
+    font.loadFromFile("SourceSansPro-Regular.otf");
+
+    sf::Text text(newYearText, font);
+    text.setCharacterSize(42);
+    text.setStyle(sf::Text::Bold);
+    text.setFillColor(sf::Color::Cyan);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(text);
+        window.display();
+    }
+
+    return 0;
+}
